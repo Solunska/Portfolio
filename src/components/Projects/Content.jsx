03a7deb0dd7ab808cf34f4ef72132d4315349482
solunska <a_solunska@outlook.com>
2024-10-01 17:12:28 +0200

    added first prject and adjusted layout and styling

--- src/components/Projects/Content.jsx
+++ src/components/Projects/Content.jsx
@@ -2,6 +2,7 @@ import Button from '../UI/Button';
 import ProjectCard from '../UI/ProjectCard';
 import styles from './Content.module.css';
 import cover from '../../assets/cover.png'
+import cover1 from '../../assets/cover1.png'
 
 export default function Content(){
     return <div className={styles.mainContainer}>
@@ -11,22 +12,22 @@ export default function Content(){
         </div>
         <div className={styles.projectsContainer}>
             <ProjectCard 
-            description="Lorem ipsum dolor sit amet consectetur. Cras tortor a consectetur pellentesque diam eu. Varius eget amet parturient viverra diam nulla velit pellentesque etiam. Velit donec tellus nulla aenean mi cras mattis cras sed. Diam consectetur eget tristique tincidunt mauris egestas ullamcorper nunc et. Amet elementum cursus sem auctor sem mus amet. Ac placerat eu suscipit iaculis a dui ac aliquet ac. Velit congue sodales est nulla eros tempor mauris sit." 
+            description="Nabi E-Shop is a modern and responsive e-commerce platform built with React that offers a seamless shopping experience for users. The application features a user-friendly interface, allowing customers to browse and purchase a variety of products effortlessly. Key functionalities include product listings with detailed descriptions, a shopping cart for easy order management, and secure user authentication. " 
             linkCode="https://github.com/Solunska/e-shop-in-react" 
             linkDemo="https://nabistore-fcbe3.web.app/" 
             photo={cover} 
+            projectName="Nabi E-Shop"  />
+            <ProjectCard 
+            description="Lorem ipsum dolor sit amet consectetur. Cras tortor a consectetur pellentesque diam eu. Varius eget amet parturient viverra diam nulla velit pellentesque etiam. Velit donec tellus nulla aenean mi cras mattis cras sed. Diam consectetur eget tristique tincidunt mauris egestas ullamcorper nunc et. Amet elementum cursus sem auctor sem mus amet. Ac placerat eu suscipit iaculis a dui ac aliquet ac. Velit congue sodales est nulla eros tempor mauris sit." 
+            linkCode="https://github.com/Solunska/e-shop-in-react" 
+            linkDemo="https://nabistore-fcbe3.web.app/" 
+            photo={cover1} 
             projectName="E Shop in React"  />
             <ProjectCard 
             description="Lorem ipsum dolor sit amet consectetur. Cras tortor a consectetur pellentesque diam eu. Varius eget amet parturient viverra diam nulla velit pellentesque etiam. Velit donec tellus nulla aenean mi cras mattis cras sed. Diam consectetur eget tristique tincidunt mauris egestas ullamcorper nunc et. Amet elementum cursus sem auctor sem mus amet. Ac placerat eu suscipit iaculis a dui ac aliquet ac. Velit congue sodales est nulla eros tempor mauris sit." 
             linkCode="https://github.com/Solunska/e-shop-in-react" 
             linkDemo="https://nabistore-fcbe3.web.app/" 
-            photo={cover} 
-            projectName="E Shop in React"  />
-            <ProjectCard 
-            description="Lorem ipsum dolor sit amet consectetur. Cras tortor a consectetur pellentesque diam eu. Varius eget amet parturient viverra diam nulla velit pellentesque etiam. Velit donec tellus nulla aenean mi cras mattis cras sed. Diam consectetur eget tristique tincidunt mauris egestas ullamcorper nunc et. Amet elementum cursus sem auctor sem mus amet. Ac placerat eu suscipit iaculis a dui ac aliquet ac. Velit congue sodales est nulla eros tempor mauris sit." 
-            linkCode="https://github.com/Solunska/e-shop-in-react" 
-            linkDemo="https://nabistore-fcbe3.web.app/" 
-            photo={cover} 
+            photo={cover1} 
             projectName="E Shop in React"  />
         </div>
     </div>
--- src/components/Resume/Content.jsx
+++ src/components/Resume/Content.jsx
@@ -11,20 +11,24 @@ import javaScriptImg from '../../assets/javaScript.png';
 import bootstrapImg from '../../assets/bootstrap.png';
 import reactImg from '../../assets/react.png';
 
-const content = 'Hardworking, ambitious, and creative college student with a keen interest in UX/UI design and development. Equipped with a solid understanding of web technologies, including HTML, CSS, JavaScript, and Figma, I specialize in crafting visually pleasing web applications. As a quick learner, I am ready to take on any challenge and eager to increase my knowledge while gaining practical experience. Seeking an opportunity to apply my skills and committed to becoming a dependable and valuable team member, I aim to contribute effectively to projects, deliver innovative solutions, and further develop my expertise in UX/UI design and development.';
+const content = ['Hardworking, ambitious, and creative college student with a keen interest in UX/UI design and development. Equipped with a solid understanding of web technologies, including HTML, CSS, JavaScript, and Figma, I specialize in crafting visually pleasing web applications. As a quick learner, I am ready to take on any challenge and eager to increase my knowledge while gaining practical experience. Seeking an opportunity to apply my skills and committed to becoming a dependable and valuable team member, I aim to contribute effectively to projects, deliver innovative solutions, and further develop my expertise in UX/UI design and development.', 'I’m actively seeking internship opportunities where I can apply my technical skills, creative problem-solving, and collaborative spirit to contribute to real-world projects. I thrive in environments where I can learn from experienced professionals, tackle challenges, and grow as a developer and designer.'];
+
 
 export default function Content() {
     return <div className={styles.resume_container}>
         <div className={styles.resume_introduction}>
-            <Introduction />
-            <div className={styles.resume_content}>{content}</div>
+            <Introduction hiddenParagraph={true} />
+            <div className={styles.resume_content}>{content[0]}</div>
+            <div className={styles.resume_content}>{content[1]}</div>
         </div>
         <div className={styles.education_and_skills_container}>
             <p className={styles.label}>Education</p>
-            <Education school='Faculty of Computer Science and Engineering, Skopje'
-                year='2020 - Present' />
-            <Education school='Gymnasium Kocho Racin, Veles'
-                year='2016 - 2020' />
+            <div>
+                <Education school='Faculty of Computer Science and Engineering, Skopje'
+                    year='2020 - Present' />
+                <Education school='Gymnasium Kocho Racin, Veles'
+                    year='2016 - 2020' />
+            </div>
             <div className={styles.skills}>
                 <Skill image={htmlImg} alt='html' />
                 <Skill image={cssImg} alt='css' />
@@ -32,6 +36,9 @@ export default function Content() {
                 <Skill image={bootstrapImg} alt='bootstrap' />
                 <Skill image={figmaImg} alt='figma' />
                 <Skill image={reactImg} alt='react' />
+                <Skill image={githubImg} alt='github' />
+                <Skill image={githubImg} alt='github' />
+                <Skill image={githubImg} alt='github' />
                 <Skill image={githubImg} alt='github' />
             </div>
             <div className={styles.aditional_skills_container}>
