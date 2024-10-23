b51ab8d92df20fe57ee55d23dc24499e21d58c4d
solunska <a_solunska@outlook.com>
2024-10-23 14:57:07 +0200

    fixed button

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -2,13 +2,14 @@ import './Button.css';
 import { motion } from 'framer-motion';
 
 export default function Button({ children, buttonClass, handleClick, linkto }) {
-    return <motion.button
-        onClick={handleClick}
-        className={buttonClass}
-        initial={{ scale: 0.8 }}
-        animate={{ scale: 1 }}
-        exit={{ scale: 0.8 }}
-        transition={{ type: 'spring', stiffness: 50 }}>
-        <a href={linkto}>{children}</a>
-    </motion.button>
+    return <a href={linkto}>
+        <motion.button
+            onClick={handleClick}
+            className={buttonClass}
+            initial={{ scale: 0.8 }}
+            animate={{ scale: 1 }}
+            exit={{ scale: 0.8 }}
+            transition={{ type: 'spring', stiffness: 50 }}>
+            {children}
+        </motion.button></a>
 }
